f73a7077ac575431d1ae5178b0e8a437e3d9fc71
Mayanksikarwar18 <mayanksikarwar1999@gmail.com>
2025-12-23 00:59:50 +0530

    home page created

--- app/page.js
+++ app/page.js
@@ -1,15 +1,22 @@
 
+import Navbar from "@/components/Navbar"
+
+
 export default function Home() {
   return (
     <>
-      {/* <div className="flex h-screen ">
-        <div className="text-black bg-white h-full w-full flex justify-center items-center">
-          <h1 className="font-bold text-9xl">A link in bio built for you.</h1>
+      <div className="h-screen bg-[#d2e823]">
+        <div className="pt-10"><Navbar /></div>
+        <div className="flex ">
+          <div className="text-black h-full w-1/2 flex flex-col justify-end items-center">
+            <h1 className="text-[80px] text-start ml-20 mt-40 text-[#254f1a] text-base/20  tracking-tighter ExtraBold800">A link in bio built for you.</h1>
+            <p className="text-xl text-start ml-20 mt-10   text-[#254f1a] ExtraBold400 ">Join 70M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+          </div>
+          <div className="bg-gray-200 h-full w-1/2" >
+
+          </div>
         </div>
-        <div className="bg-gray-200 h-full w-full" >
-          fbsjbsjkb
-        </div>
-      </div> */}
+      </div>
     </>
   )
 }
