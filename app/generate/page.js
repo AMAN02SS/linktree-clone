20815efe2c342d42af07407530bbcc58e943feb5
AMAN02SS <7000602733aman@gmail.com>
2025-12-29 23:30:38 +0530

    29-12-25

--- app/generate/page.js
+++ app/generate/page.js
@@ -3,17 +3,17 @@ import React from 'react'
 const Generate = () => {
     return (
         <>
-            <div className='absolute top-10 left-10'>
+            <div className='absolute top-10 left-10 z-10'>
                 <div className='flex justify-start items-baseline pl-5'>
                     <h1 className='text-4xl font-bold'>Linktree</h1><img className='' width={25} height={25} src='/Pic/logo.svg' />
                 </div>
             </div>
-            <div className='h-screen w-full flex overflow-hidden relative'>
+            <div className='h-screen w-full flex overflow-hidden relative bg-[#225ac0]'>
                 <div className='h-full w-1/2 flex flex-col justify-center items-center'>
-                    <div className='pt-30'>
-                        <div className='text-3xl font-bold'>Welcome to Linktree</div>
-                        <div>Create your Link today!</div>
-                        <div className='flex flex-col items-center justify-center gap-4 py-10'>
+                    <div className='py-10 bg-[#c2d816] rounded-2xl px-15'>
+                        <div className='text-3xl font-bold text-center'>Welcome to Linktree</div>
+                        <div className='text-center py-2'>Create your Linktee today!</div>
+                        <div className='flex flex-col items-center justify-center gap-4'>
                             <div className='flex flex-col items-center justify-center gap-2'>
                                 <label className='text-xl font-semibold' htmlFor="">Step 1: Claim your Handle</label>
                                 <input className='border p-2 w-100 bg-gray-300 rounded-lg' type="text" placeholder='Chose your handle' />
@@ -26,7 +26,7 @@ const Generate = () => {
                             </div>
                             <div className='flex flex-col items-center justify-centergap-2'>
                                 <label className='text-xl font-semibold' htmlFor="">Step 3: Add Picture and Finalize</label>
-                                <input className='border p-2  w-100 bg-gray-300 rounded-lg' type="text" placeholder='Enter link of Picture' />
+                                <input className='border p-2 w-100 bg-gray-300 rounded-lg' type="text" placeholder='Enter link of your Picture' />
                             </div>
                             <button className='bg-[#1e2330] font-semibold text-white rounded-full py-4 px-8 hover:bg-[#424754]'>Create Your LinkTree</button>
                         </div>
